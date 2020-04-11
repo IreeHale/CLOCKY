fafa2a1593192ac321cbea5a707f259d143d03c4
Iree Hale <KidFLash2099@protonmail.com>
2020-04-10 19:03:21 -0700

    adding js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+const fullDate = new Date();
+const hours = fullDate.getHours();
+const mins = fullDate.getMinutes();
+const secs = fullDate.getSeconds();
+
+document.getElementById('hour').innerHTML = hours;
+document.getElementById('minute').innerHTML = mins;
+document.getElementById('second').innerHTML = secs;
